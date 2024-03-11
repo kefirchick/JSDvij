77d8e35f06b4394e45b00eb5136dc9fce352b5ff
MedRug <i.kustov@medrug.kz>
2024-03-11 11:46:24 +0500

    init_commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let frameRate = 20;
+let x = 0;
+let y = 0;
+let input = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+}
+
+let player = document.createElement('div');
+player.className = "player";
+document.body.append(player);
+let playerRect = player.getBoundingClientRect();
+
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'ArrowLeft') input.left = true;
+    if (event.key == 'ArrowRight') input.right = true;
+    if (event.key == 'ArrowUp') input.up = true;
+    if (event.key == 'ArrowDown') input.down = true;
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'ArrowLeft') input.left = false;
+    if (event.key == 'ArrowRight') input.right = false;
+    if (event.key == 'ArrowUp') input.up = false;
+    if (event.key == 'ArrowDown') input.down = false;
+});
+
+let timerId = setInterval(update, 1000/frameRate);
+
+function update() {
+    if (input.left) x--;
+    if (input.right) x++;
+    if (input.up) y--;
+    if (input.down) y++;
+    player.style.left = x + 'px';
+    div.style.top = y + 'px';
+}
