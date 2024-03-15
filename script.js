4a2c505f5b723dcb04ae85b3d628a2f98e6465c8
MedRug <i.kustov@medrug.kz>
2024-03-15 10:27:49 +0500

    collision test functions

--- script.js
+++ script.js
@@ -10,14 +10,54 @@ class Level {
 
     createActor(x, y, w, h, name, color) {
         let actor = new Actor(x, y, w, h, name, color);
-        this.actors.name = actor;
+        this.actors[name] = actor;
         return actor;
     }
 
     update() {
-        for (let actor in this.actors) {
-            if (!this.isCollision(actor)) return;
+        this.player.update();
+        // сюда писать проверки разностей координат чтоб найти направление коллизии
+        if (this.actors.test) {
+            this.test(this.player.x, this.player.x+this.player.w,
+                      this.player.y, this.player.y+this.player.h,
+                      this.actors.test.x, this.actors.test.x+this.actors.test.w,
+                      this.actors.test.y, this.actors.test.y+this.actors.test.h);
+            this.test2(this.player.x + this.player.w - this.actors.test.x,
+                       this.actors.test.x + this.actors.test.w - this.player.x,
+                       this.player.y + this.player.h - this.actors.test.y,
+                       this.actors.test.y + this.actors.test.h - this.player.y);
         }
+
+        // for (let actor in this.actors) {
+        //     if (!this.isCollision(actor)) return;
+        // }
+    }
+
+    test(ax1, ax2, ay1, ay2, bx1, bx2, by1, by2) {
+        // console.log("ax1, ax2, ay1, ay2, bx1, bx2, by1, by2:", ax1, ax2, ay1, ay2, bx1, bx2, by1, by2);
+        console.log("ax2-bx1:", ax2-bx1, "bx2-ax1", bx2-ax1, "ay2-by1:", ay2-by1, "by2-ay1", by2-ay1);
+    }
+
+    test2(dLeft, dRight, dUp, dDown) {
+        //dLeft = ax2-bx1, dRight = bx2-ax1, dUp = ay2-by1, dDown = by2-ay1
+        console.log(dLeft, dRight, dUp, dDown);
+        if (dLeft<0 || dRight<0 || dUp<0 || dDown<0) {
+            console.log("No collision");
+            return;
+        }
+        if (dLeft <= dRight && dLeft <= dUp && dLeft <= dDown) {
+            console.log("Right");
+            return;
+        }
+        if (dRight <= dUp && dRight <= dDown) {
+            console.log("Left");
+            return;
+        }
+        if (dUp <= dDown) {
+            console.log("Down");
+            return;
+        }
+        console.log("Up");
     }
 
     isCollision(actor) {
@@ -107,10 +147,11 @@ let border1 = level.createActor(10, 10, 10, 320, 'border1', 'blue');
 level.createActor(20, 10, 300, 10, 'border2', 'blue');
 level.createActor(320, 10, 10, 320, 'border3', 'blue');
 level.createActor(20, 320, 300, 10, 'border4', 'blue');
+level.createActor(100, 100, 100, 100, 'test', 'green');
 
 
 let timerId = setInterval(update, 1000/frameRate);
 
 function update() {
-    player.update();
+    level.update();
 }
